dd4645ec5c4f0ef9ddd33121b7481b67689d2198
Kunal Hole <kunalhole538@gmail.com>
2022-08-06 22:31:08 +0530

    Header and Home Page is Completed

---
+++ pages/404.jsx
@@ -0,0 +1,7 @@
+const Error = () => {
+  return (
+    <h1 className="font-bold text-xl text-center">Net Search kar na Manda</h1>
+  )
+}
+
+export default Error
---
+++ pages/search.jsx
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+const Search = () => {
+  return (
+    <div>
+      <Head>
+        <title>Search Results</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <h1>Search Page</h1>
+
+      {/* Body */}
+    </div>
+  )
+}
+
+export default Search
